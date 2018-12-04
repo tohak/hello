0db050b9ccb91df800c401bfc5e1fc550686a5cd
Антон <tohak199@gmail.com>
2018-12-04 14:19:12 +0200

    clean code

--- src/test/java/com/konovalov/hello/service/ContactsServiceTest.java
+++ src/test/java/com/konovalov/hello/service/ContactsServiceTest.java
@@ -3,17 +3,14 @@ package com.konovalov.hello.service;
 import com.konovalov.hello.domain.Contacts;
 import com.konovalov.hello.repository.ContactsRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,11 +26,15 @@ public class ContactsServiceTest {
     @Test
     public void getByNameFilter() {
         String filter = "^A.*$";
-        Mockito.doReturn(Arrays.asList(new Contacts("Anton")))
-                .when(contactsService)
-                .getAll();
-        List<Contacts> contactsList = contactsService.getByNameFilter(filter);
-        Assert.assertNotNull(contactsList);
+        List<Contacts> contactsList = Arrays.asList(new Contacts("Anton"), new Contacts("Vlad"));
+        Mockito.doReturn(contactsList)
+                .when(contactsRepository)
+                .findAll();
+
+        List<Contacts> result = contactsService.getByNameFilter(filter);
+        Mockito.verify(contactsRepository, Mockito.times(1)).findAll();
+        Assert.assertEquals(result.size(), 1);
+        Assert.assertEquals(result.get(0).getName(), "Anton");
     }
 
     @Test
@@ -50,6 +51,9 @@ public class ContactsServiceTest {
     public void badCreateContact() {
         String contactName = "Vala";
 
+        Mockito.doReturn(new Contacts())
+                .when(contactsRepository)
+                .findByName("Vala");
         Mockito.doReturn(new Contacts())
                 .when(contactsRepository)
                 .findByName("Vala");
